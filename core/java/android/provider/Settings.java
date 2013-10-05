27bfd1677effdfb0b56fe148db63a1480cd5bb18
maxwen <max.weninger@gmail.com>
2013-10-05 13:15:36 +0200

    base: add support for adb over network [1/3]
    Change-Id: If8354c92a03da501c683638cc69097d287d74816

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -15387,6 +15387,12 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * The TCP/IP port to run ADB on, or 0 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
