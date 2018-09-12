a54c796be40da7e09b6bdf88d27b4fa9273bf0f6
Mazda-- <alex@dirtyunicorns.com>
2018-09-12 21:47:28 +0700

    Lockscreen Clock Widget Font Style [1/2]
    @neobuddy89:
    * Code clean up and use list of fonts from oreo to pie.
    
    @rohanpurohit:
    * rewrite for 10
    
    Change-Id: I963e96ed74bea1f46dcd69703b5ab226a4801853
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5494,6 +5494,12 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER_FONT_STYLE = "status_bar_carrier_font_style";
 
         /**
+         * Change font style for the system lockscreen clock widget
+         * @hide
+         */
+        public static final String LOCK_CLOCK_FONT_STYLE = "lock_clock_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
