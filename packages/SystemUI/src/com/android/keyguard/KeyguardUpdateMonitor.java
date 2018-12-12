3786b9bb69a3c14523a5fce4a4176eab6adf9d0e
ZeNiXxX <vitjahermann@gmail.com>
2018-12-12 12:18:43 +0100

    pocket: Allow to listen for fingerprint
    Change-Id: I49a32796b031c3123d9bb267ef97de1ecd9a38a7
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1797,7 +1797,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
                     (mBouncer && !mKeyguardGoingAway) || mGoingToSleep ||
                     shouldListenForFingerprintAssistant() || (mKeyguardOccluded && mIsDreaming))
                     && !mSwitchingUser && !isFingerprintDisabled(getCurrentUser())
-                    && (!mKeyguardGoingAway || !mDeviceInteractive) && mIsPrimaryUser && !mIsDeviceInPocket;
+                    && (!mKeyguardGoingAway || !mDeviceInteractive) && mIsPrimaryUser;
         }
     }
 
