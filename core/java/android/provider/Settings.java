bdd12516591dacf33cc30cf1fb286b8b8ba63f9d
Pranav Vashi <neobuddy89@gmail.com>
2019-03-19 05:22:13 +0700

    Lockscreen Date Font Style [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5512,6 +5512,12 @@ public final class Settings {
         public static final String LOCKSCREEN_DATE_SELECTION = "lockscreen_date_selection";
 
         /**
+         * Change font style for the system lockscreen date
+         * @hide
+         */
+        public static final String LOCK_DATE_FONT_STYLE = "lock_date_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
