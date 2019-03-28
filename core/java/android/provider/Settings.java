2ee6ba0f7ff51ccef8a83b56c8d3f062ac729028
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    @ZeNiXxX - Bring to Android 10
    
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>
    Signed-off-by: Kry9toN <dhimasbagusprayoga@gmail.com>
    Signed-off-by: Reza Adi Pangestu <rezaadipangestu5@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5109,6 +5109,16 @@ public final class Settings {
         public static final String BATTERY_FULLY_CHARGED_NOTIF = "battery_fully_charged_notif";
 
         /**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
