8ecfbeb272cdc50e40dde70a18825a1ff9b3bde4
maxwen <max.weninger@gmail.com>
2019-12-21 18:31:26 +0200

    base: wake from aod on unplug if we show on charging only
    so we go back in non aod mode correctly
    
    Change-Id: I7e0fdecbf549a8a631090a00ffb6b29fac374e8a
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -2088,7 +2088,9 @@ public final class PowerManagerService extends SystemService
 
         // On Always On Display, SystemUI shows the charging indicator
         if (mAlwaysOnEnabled && mWakefulness == WAKEFULNESS_DOZING) {
-            return false;
+            if (!mDozeOnChargeEnabled) {
+                return false;
+            }
         }
 
         // Otherwise wake up!
