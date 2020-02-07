34db3cf5b2eb3d71bb4b742058317b49116825e5
AnkitGourav <Ankitgourav035@gmail.com>
2020-02-07 13:19:24 +0530

    SystemUI: Improve edge pulse
    - fix edge light on doze triggering false AoD pulse
    - fix AoD pulsing even after disabling Edge light
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1031,6 +1031,9 @@ public final class PowerManagerService extends SystemService
                 Settings.Global.WAKELOCK_BLOCKING_LIST),
                 false, mSettingsObserver, UserHandle.USER_ALL);
         resolver.registerContentObserver(Settings.System.getUriFor(
+                Settings.System.PULSE_AMBIENT_LIGHT),
+                false, mSettingsObserver, UserHandle.USER_ALL);
+        resolver.registerContentObserver(Settings.System.getUriFor(
                 Settings.System.AMBIENT_NOTIFICATION_LIGHT),
                 false, mSettingsObserver, UserHandle.USER_ALL);
         resolver.registerContentObserver(Settings.System.getUriFor(
@@ -1159,19 +1162,22 @@ public final class PowerManagerService extends SystemService
         String blockedWakelockList = Settings.Global.getString(resolver,
                 Settings.Global.WAKELOCK_BLOCKING_LIST);
         setBlockedWakeLocks(blockedWakelockList);
-        boolean mAmbientLights = Settings.System.getIntForUser(
-                mContext.getContentResolver(), Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED,
-                0, UserHandle.USER_CURRENT) != 0;
-        if (mAmbientLights) {
-            boolean dozeOnNotification = Settings.System.getIntForUser(resolver,
-                    Settings.System.AMBIENT_NOTIFICATION_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
-            Settings.System.putIntForUser(mContext.getContentResolver(),
-                     Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, dozeOnNotification ? 1 : 0,
-                     UserHandle.USER_CURRENT);
-        } else {
-             Settings.System.putIntForUser(mContext.getContentResolver(),
-                     Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, 0,
-                     UserHandle.USER_CURRENT);
+        boolean pulseLights = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_AMBIENT_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
+        boolean mAmbientLights = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
+        if (pulseLights) {
+            if (mAmbientLights) {
+                boolean dozeOnNotification = Settings.System.getIntForUser(resolver,
+                        Settings.System.AMBIENT_NOTIFICATION_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
+                Settings.System.putIntForUser(mContext.getContentResolver(),
+                         Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, dozeOnNotification ? 1 : 0,
+                         UserHandle.USER_CURRENT);
+            } else {
+                Settings.System.putIntForUser(mContext.getContentResolver(),
+                         Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, 0,
+                         UserHandle.USER_CURRENT);
+            }
         }
         // depends on AMBIENT_NOTIFICATION_LIGHT_ACTIVATED - so MUST be afterwards
         // no need to call us again
