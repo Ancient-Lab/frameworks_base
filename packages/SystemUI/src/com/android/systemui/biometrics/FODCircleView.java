57c7c7c95be820c4bec02601da2527acc084a4cb
Hxmg <mzangoose@gmail.com>
2020-02-21 17:52:16 +0800

    SystemUI: Add ColorOS7 recognizing FP animations [1/2]
    Change-Id: I81e149e8aedd11613c487b530e23ca76567161be
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -641,7 +641,9 @@ class FODAnimation extends ImageView {
         R.drawable.fod_op_cosmos_recognizing_anim,
         R.drawable.fod_op_mclaren_recognizing_anim,
         R.drawable.fod_pureview_future_recognizing_anim,
-        R.drawable.fod_pureview_molecular_recognizing_anim
+        R.drawable.fod_pureview_molecular_recognizing_anim,
+        R.drawable.fod_coloros7_1_recognizing_anim,
+        R.drawable.fod_coloros7_2_recognizing_anim
     };
 
     public FODAnimation(Context context, int mPositionX, int mPositionY) {
