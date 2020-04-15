097cbe802b125df0853c122f3f03333cd6521144
rezaadi0105 <rezaadipangestu5@gmail.com>
2020-04-15 12:09:12 +0700

    Revert "Refresh QS drag handle look"
    This reverts commit 9cda26a0bc2ac1451f98fbeedf2bcd289de31785.

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -232,6 +232,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         return new TouchAnimator.Builder()
                 .addFloat(mActionsContainer, "alpha", 0, 1)
                 .addFloat(mEditContainer, "alpha", 0, 1)
+                .addFloat(mDragHandle, "alpha", 1, 0, 0)
                 .addFloat(mPageIndicator, "alpha", 0, 1)
                 .setStartDelay(0.15f)
                 .build();
