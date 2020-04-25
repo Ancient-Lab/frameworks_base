382f7c00bf724d90f177b55bb1c9745a7f66d16f
rezaadi0105 <rezaadipangestu5@gmail.com>
2020-04-25 09:42:25 +0000

    Lockscreen Clock Widget Font Style [1/2]
    @neobuddy89:
    * Code clean up and use list of fonts from oreo to pie.
    
    @rohanpurohit:
    * rewrite for 10
    
    Change-Id: I963e96ed74bea1f46dcd69703b5ab226a4801853
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5499,6 +5499,12 @@ public final class Settings {
          */
         public static final String DISMISS_ALL_BUTTON = "dismiss_all_button";
 
+	/**
+         * Change font style for the system lockscreen clock widget
+         * @hide
+         */
+        public static final String LOCK_CLOCK_FONT_STYLE = "lock_clock_font_style";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
