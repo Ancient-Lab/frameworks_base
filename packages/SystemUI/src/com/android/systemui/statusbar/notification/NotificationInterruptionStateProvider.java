b0c08fa04d4b2a9baa042c9728ff598abb8ac270
rituj <ritujbeniwal@gmail.com>
2020-04-30 19:13:56 +0530

    base: Introduce Applock [1/2]
    * Built upon the initial commit by "Anass Karbila <anaskarbila@aospa.co>"
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Change-Id: Ida21d22035d708a359eaaa29e2c48ff6f0dd6627

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
@@ -170,6 +170,10 @@ public class NotificationInterruptionStateProvider {
      * @return true if the entry should bubble up, false otherwise
      */
     public boolean shouldBubbleUp(NotificationEntry entry) {
+        if (entry.rowExists() && entry.getRow().isAppLocked()) {
+            return false;
+        }
+
         final StatusBarNotification sbn = entry.notification;
 
         if (!canAlertCommon(entry)) {
@@ -222,6 +226,11 @@ public class NotificationInterruptionStateProvider {
     }
 
     private boolean shouldHeadsUpWhenAwake(NotificationEntry entry) {
+        if (mStatusBarStateController.getState() != StatusBarState.KEYGUARD
+                && entry.rowExists() && entry.getRow().blockHeadsUp()) {
+            return false;
+        }
+
         StatusBarNotification sbn = entry.notification;
 
         if (!mUseHeadsUp) {
--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -160,6 +160,7 @@ import com.android.server.usage.UsageStatsService;
 import com.android.server.vr.VrManagerService;
 import com.android.server.webkit.WebViewUpdateService;
 import com.android.server.wm.ActivityTaskManagerService;
+import com.android.server.wm.AppLockService;
 import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
 
@@ -1113,6 +1114,10 @@ public final class SystemServer {
 
             traceBeginAndSlog("PinnerService");
             mSystemServiceManager.startService(PinnerService.class);
+            traceEnd();
+
+            traceBeginAndSlog("StartAppLockService");
+            mSystemServiceManager.startService(AppLockService.class);
             traceEnd();
 
             traceBeginAndSlog("SignedConfigService");
