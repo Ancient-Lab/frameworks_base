e829036ad0cd01464501a79019165c033126a3a2
shivatejapeddi <shivatejapeddi@gmail.com>
2020-05-30 14:18:28 +0000

    Utils: Add isPackageAvailable method
    Change-Id: I6d97e18ec4fc260264145caadafa81792ff3e8e8
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- core/java/com/android/internal/util/ancient/Utils.java
+++ core/java/com/android/internal/util/ancient/Utils.java
@@ -367,6 +367,18 @@ public class Utils {
         }
     }
 
+    public static boolean isPackageAvailable(Context context, String packageName) {
+        final PackageManager pm = context.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+
     // Check if device has a notch
     public static boolean hasNotch(Context context) {
         int result = 0;
