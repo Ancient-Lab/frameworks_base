269085ffea3e843f15d9ab3abf74f8e89242e146
karthick111 <karthickrko61@gmail.com>
2020-07-29 04:54:33 +0000

    BatteryService: Add VOOC charging support
    Based On: ForkLineageOS/android_frameworks_base@4ed74dc
    
    Signed-off-by: TH3GUNN3R <bibhuti.gamer@gmail.com>
    Signed-off-by: Shubham <skpawar1305@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -77,9 +77,12 @@ import com.android.server.am.BatteryStatsService;
 import com.android.server.lights.Light;
 import com.android.server.lights.LightsManager;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayDeque;
@@ -187,6 +190,10 @@ public final class BatteryService extends SystemService {
     private boolean mDashCharger;
     private boolean mLastDashCharger;
 
+    private boolean mVoocCharger;
+    private boolean mHasVoocCharger;
+    private boolean mLastVoocCharger;
+   
     private long mDischargeStartTime;
     private int mDischargeStartLevel;
 
@@ -226,6 +233,9 @@ public final class BatteryService extends SystemService {
         mBatteryStats = BatteryStatsService.getService();
         mActivityManagerInternal = LocalServices.getService(ActivityManagerInternal.class);
         mNotificationManager = mContext.getSystemService(NotificationManager.class);
+
+        mHasVoocCharger = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_hasVoocCharger);
 
         mCriticalBatteryLevel = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_criticalBatteryWarningLevel);
@@ -576,6 +586,7 @@ public final class BatteryService extends SystemService {
 
         mDashCharger = Utils.isDashCharger();
 
+        mVoocCharger = mHasVoocCharger && isVoocCharger();
         if (force || (mHealthInfo.batteryStatus != mLastBatteryStatus ||
                 mHealthInfo.batteryHealth != mLastBatteryHealth ||
                 mHealthInfo.batteryPresent != mLastBatteryPresent ||
@@ -587,7 +598,8 @@ public final class BatteryService extends SystemService {
                 mHealthInfo.maxChargingVoltage != mLastMaxChargingVoltage ||
                 mHealthInfo.batteryChargeCounter != mLastChargeCounter ||
                 mInvalidCharger != mLastInvalidCharger ||
-                mDashCharger != mLastDashCharger)) {
+                mDashCharger != mLastDashCharger ||
+                mVoocCharger != mLastVoocCharger )) {
 
             if (mPlugType != mLastPlugType) {
                 if (mLastPlugType == BATTERY_PLUGGED_NONE) {
@@ -765,7 +777,8 @@ public final class BatteryService extends SystemService {
             mLastBatteryLevelCritical = mBatteryLevelCritical;
             mLastInvalidCharger = mInvalidCharger;
             mLastDashCharger = mDashCharger;
-        }
+            mLastVoocCharger = mVoocCharger; 
+           }
     }
 
     private void sendBatteryChangedIntentLocked() {
@@ -793,6 +806,7 @@ public final class BatteryService extends SystemService {
         intent.putExtra(BatteryManager.EXTRA_MAX_CHARGING_VOLTAGE, mHealthInfo.maxChargingVoltage);
         intent.putExtra(BatteryManager.EXTRA_CHARGE_COUNTER, mHealthInfo.batteryChargeCounter);
         intent.putExtra(BatteryManager.EXTRA_DASH_CHARGER, mDashCharger);
+        intent.putExtra(BatteryManager.EXTRA_VOOC_CHARGER, mVoocCharger);
         if (DEBUG) {
             Slog.d(TAG, "Sending ACTION_BATTERY_CHANGED. scale:" + BATTERY_SCALE +
                     ", info:" + mHealthInfo.toString() +
@@ -1203,6 +1217,20 @@ public final class BatteryService extends SystemService {
     private boolean shouldClearBatteryFullyChargedNotificationLocked() {
         return mIsShowingBatteryFullyChargedNotification &&
                 (mPlugType == 0 || mHealthInfo.batteryLevel < BATTERY_SCALE);
+    }
+
+    private boolean isVoocCharger() {
+        try {
+            FileReader file = new FileReader("/sys/class/power_supply/battery/voocchg_ing");
+            BufferedReader br = new BufferedReader(file);
+            String state = br.readLine();
+            br.close();
+            file.close();
+            return "1".equals(state);
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+        return false;
     }
 
     private void clearBatteryFullyChargedNotificationLocked() {
