70d641399093e3f266accf6c044a1de07bbff948
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-08-24 12:03:01 +0000

    Clock Style : Remove accent color conditions and add few ZenX  clocks
    based on:
    https://github.com/ZenX-OS/android_frameworks_base/commit/eb81c3feb60b0e1464b640f0cf859882039b4a55
    https://github.com/ZenX-OS/android_frameworks_base/commit/e7a35bd41b403fd79fba8f5fc0c1fb5f2adbd2b8
    
    Co-authored-by: Shubham <skpawar1305@gmail.com>
    Change-Id: I8e79659f5aaa11ce43624aad7c90f733aadf7b2a

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -160,6 +160,8 @@ public final class ClockManager {
         addBuiltinClock(() -> new OronosClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new MNMLBoxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new BubbleAnimationClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new SwingClockController(res, layoutInflater, colorExtractor, context));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
