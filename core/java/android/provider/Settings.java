a3187cff6099bc095dd40a038b6fc85e9ff62998
Steve Kondik <steve@cyngn.com>
2020-08-27 23:38:06 +0700

    SystemUI: Forward-port notification counters [1/2]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6714,6 +6714,12 @@ public final class Settings {
         public static final String QS_TILE_ICON_PRIMARY = "qs_tile_icon_primary";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
